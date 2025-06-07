e4c0f215434eeda64cc3950495da2830ff26d53e
nyehamene <nhcdeveloper@gmail.com>
2025-06-07 18:10:39 -0400

    (format) javascript (grammar.js)

--- grammar.js
+++ grammar.js
@@ -10,91 +10,86 @@
 module.exports = grammar({
   name: "teml",
 
-  word: $ => $._identifier_simple,
+  word: ($) => $._identifier_simple,
 
-  extras: $ => [$.comment, /\s/],
+  extras: ($) => [$.comment, /\s/],
 
   rules: {
-    source_file: $ => seq(
-      // NOTE: package is optional for testing only
-      optional($.package_declaration),
-      optional(
-        seq(
-          $._imports,
-          repeat($.using_declaration)
-        )
+    source_file: ($) =>
+      seq(
+        // NOTE: package is optional for testing only
+        optional($.package_declaration),
+        optional(seq($._imports, repeat($.using_declaration))),
       ),
-    ),
 
     comment: () => token(seq("//", /[^\n]*/)),
 
     _identifier_simple: () => /[a-zA-Z][a-zA-Z-_]*/,
 
-    _identifier_qualified: $ => seq(
-      $._identifier_simple,
-      repeat1(
-        seq(
-          token.immediate("/"),
-          /[^\s\d-]/,
-          $._identifier_simple
-        )
-      )
-    ),
+    _identifier_qualified: ($) =>
+      seq(
+        $._identifier_simple,
+        repeat1(seq(token.immediate("/"), /[^\s\d-]/, $._identifier_simple)),
+      ),
 
-    identifier: $ => choice(
-      alias($._identifier_simple, "simple"),
-      alias($._identifier_qualified, "qualified")
-    ),
+    identifier: ($) =>
+      choice(
+        alias($._identifier_simple, "simple"),
+        alias($._identifier_qualified, "qualified"),
+      ),
 
     // TODO: support escape sequences are expression; may require external scanner
-    _string_quoted: () => token(seq("\"", /[^"]*/, "\"")),
+    _string_quoted: () => token(seq('"', /[^"]*/, '"')),
 
     // TODO: support escape sequences are expression; may require external scanner
     _string_line: () => token(seq("--", /[^\n]*/)),
 
-    string: $ => choice($._string_quoted, $._string_line),
+    string: ($) => choice($._string_quoted, $._string_line),
 
-    package_declaration: $ => seq(
-      "(",
-      "package",
-      field("name", alias($._identifier_simple, $.identifier)),
-      field("path", alias($._string_quoted, $.string)),
-      ")"
-    ),
-
-    import_declaration: $ => seq(
-      "(",
-      "import",
-      field("name", alias($._identifier_simple, $.identifier)),
-      field("path", alias($._string_quoted, $.string)),
-      ")"
-    ),
-
-    _using_single: $ => seq(
-      field("name", alias($._identifier_simple, $.identifier)),
-    ),
-
-    _using_multiple: $ => seq(
-      "[",
-      optional(
-        repeat(
-          seq(
-            field("name", alias($._identifier_simple, $.identifier)),
-            optional(","), // allow trailing comma
-          )
-        )
+    package_declaration: ($) =>
+      seq(
+        "(",
+        "package",
+        field("name", alias($._identifier_simple, $.identifier)),
+        field("path", alias($._string_quoted, $.string)),
+        ")",
       ),
-      "]",
-    ),
 
-    using_declaration: $ => seq(
-      "(",
-      "using",
-      choice($._using_single, $._using_multiple),
-      field("from", alias($._identifier_simple, $.identifier)),
-      ")"
-    ),
+    import_declaration: ($) =>
+      seq(
+        "(",
+        "import",
+        field("name", alias($._identifier_simple, $.identifier)),
+        field("path", alias($._string_quoted, $.string)),
+        ")",
+      ),
 
-    _imports: $ => repeat1($.import_declaration)
-  }
+    _using_single: ($) =>
+      seq(field("name", alias($._identifier_simple, $.identifier))),
+
+    _using_multiple: ($) =>
+      seq(
+        "[",
+        optional(
+          repeat(
+            seq(
+              field("name", alias($._identifier_simple, $.identifier)),
+              optional(","), // allow trailing comma
+            ),
+          ),
+        ),
+        "]",
+      ),
+
+    using_declaration: ($) =>
+      seq(
+        "(",
+        "using",
+        choice($._using_single, $._using_multiple),
+        field("from", alias($._identifier_simple, $.identifier)),
+        ")",
+      ),
+
+    _imports: ($) => repeat1($.import_declaration),
+  },
 });
